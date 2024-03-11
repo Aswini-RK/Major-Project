309647cdc59c32189d28858bc412ee08ef5fde60
Aswini RK <111222684+Aswini-RK@users.noreply.github.com>
2024-03-11 21:22:48 +0530

    Update urls(1).py

--- Django_projects/urls(1).py
+++ Django_projects/urls(1).py
@@ -1,1 +1,9 @@
+from django.urls import path
+from . import views
 
+
+urlpatterns=[
+    path("", views.index), #My_application
+    path("<int:month>/",views.monthly_challange_by_number),
+    path("<str:month>/",views.monthly_challange,name="monthly-challenge")
+]
