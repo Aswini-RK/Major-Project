460706025cfde11b65478c4a24272464c3256234
Aswini RK <111222684+Aswini-RK@users.noreply.github.com>
2024-03-11 21:22:25 +0530

    Update urls(main).py

--- Django_projects/urls(main).py
+++ Django_projects/urls(main).py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('My_application/',include("My_application.urls"))
+    
+]
